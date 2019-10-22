4e32711d4175b5a08129430cbd7cf35c54c739a5
Travis CI <travis@travis-ci.org>
2019-10-22 00:42:03 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: 72ddede79cecb2e92927e4fbbec5e290a78a1569

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___l_s_m303_d_l_h___mag___unified.html#a6e6390616b8c1866327978e2bd5c0c56',1,'Adafruit_LSM303DLH_Mag_Unified']]]
+  ['begin',['begin',['../class_adafruit___l_s_m303_d_l_h___mag___unified.html#a3b13466cd6d87c463aa47a433ffb22f1',1,'Adafruit_LSM303DLH_Mag_Unified']]]
 ];
